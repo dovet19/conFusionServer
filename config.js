dce3c9844aa44da9c705324a3e83aab32a43e8ab
dovet <haebinlim@gmail.com>
2022-04-18 21:54:36 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/conFusion",
+  "secretKey": "12345-67890-09876-54321",
+  "mongoUrl": "mongodb://localhost:27017/conFusion",
+  "facebook": {
+    clientId: "313646134225126",
+    clientSecret: "19e8f2b040a5cac561cb2b2a1209d158",
+  },
 };
